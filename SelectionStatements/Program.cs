926355ed26dac63e4003cb18ebd06904bea281fb
jshudd <jshudd@gmail.com>
2020-04-03 13:44:38 -0500

    finished code

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -8,8 +8,9 @@ namespace SelectionStatements
         {
             //Michael's code
             var r = new Random();
-            var favNumber = r.Next(1, 1000);
+            var favNumber = r.Next(0, 1001);
 
+            Console.WriteLine("Guess a number between 0 & 1001.");
             var userInput = int.Parse(Console.ReadLine());
             
             if (userInput < favNumber)
@@ -22,7 +23,7 @@ namespace SelectionStatements
             }
             else
             {
-                Console.WriteLine($"Never mind");
+                Console.WriteLine("Never mind");
             }
         }
     }
