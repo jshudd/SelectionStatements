c99e886339e3090250dac471bcde0a2bd32ba4b2
jshudd <jshudd@gmail.com>
2020-04-03 13:39:27 -0500

    initial code attempt

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Michael's code
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+            
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high.");
+            }
+            else
+            {
+                Console.WriteLine($"Never mind");
+            }
+        }
+    }
+}
